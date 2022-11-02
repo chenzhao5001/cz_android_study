dbfcb398101770e359b0e42dff6ee0e0279db392
chenzhao <chenzhao5001@163.com>
2022-11-02 20:46:27 +0800

    kotlin_activity

---
+++ app/src/main/java/com/cz/study/test_1_activity.kt
@@ -0,0 +1,11 @@
+package com.cz.study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class test_1_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test1)
+    }
+}
