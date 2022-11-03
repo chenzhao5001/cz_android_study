b663304942aafc510200e98657f84226e45fcbaf
chenzhao <chenzhao5001@163.com>
2022-11-03 12:05:27 +0800

    调整

--- app/src/main/java/com/cz/study/Test_1_activity.kt
+++ app/src/main/java/com/cz/study/Test_1_activity.kt
@@ -1,18 +1,33 @@
 package com.cz.study
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import butterknife.ButterKnife
+import butterknife.OnClick
+import com.cz.study.fragment.MyFragment2
 
 class Test_1_activity : AppCompatActivity() {
+    var TAG: String = "Test_1_activity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test1)
-        var test: String? =  intent.getStringExtra("a")
-        Toast.makeText(this,test,Toast.LENGTH_SHORT).show();
-        getIntent()
-//        var it: Intent = Intent.getIntentOld("a")
-//        it.getStringExtra()
+        ButterKnife.bind(this)
     }
+
+
+
+    @OnClick(R.id.btn1)
+    fun onClick(view: View) {
+        Log.i(TAG,"onClick viewId=${view.id}")
+        when(view.id) {
+            R.id.btn1 -> {
+                val myFragment2 = MyFragment2()
+                getFragmentManager().beginTransaction().replace(R.id.linear_test1,myFragment2).commit()
+                Toast.makeText(this,"test",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
