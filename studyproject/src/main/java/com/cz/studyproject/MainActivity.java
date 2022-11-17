d6aea3b89fa78a02a32089d4e669dc7c1571b0a9
chenzhao <chenzhao5001@163.com>
2022-11-17 21:19:09 +0800

    沉浸式状态栏

--- studyproject/src/main/java/com/cz/studyproject/MainActivity.java
+++ studyproject/src/main/java/com/cz/studyproject/MainActivity.java
@@ -13,24 +13,15 @@ import com.cz.framework.utils.LogUtils;
 import com.cz.framework.utils.TimeUtils;
 
 public class MainActivity extends AppCompatActivity {
+    // 沉浸式 状态栏
     public void setFullscreen() {
         int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
 
         getWindow().getDecorView().setSystemUiVisibility(uiOptions);
-        //隐藏标题栏
-//        getSupportActionBar().hide();
-        setNavigationStatusColor(Color.TRANSPARENT);
-    }
-
-    public void setNavigationStatusColor(int color) {
-        //VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+        // VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
         if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            getWindow().setNavigationBarColor(color);
-            getWindow().setStatusBarColor(color);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
     }
 
