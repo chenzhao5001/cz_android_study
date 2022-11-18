f92b5abcac883e2ecafee81495e0f524da07ce1a
chenzhao <chenzhao5001@163.com>
2022-11-18 18:01:01 +0800

    调整

---
+++ studyproject/src/main/java/com/cz/studyproject/ui/GuideActivity.java
@@ -0,0 +1,14 @@
+package com.cz.studyproject.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.cz.studyproject.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
---
+++ studyproject/src/main/java/com/cz/studyproject/ui/IndexActivity.java
@@ -0,0 +1,14 @@
+package com.cz.studyproject.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.cz.studyproject.R;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
