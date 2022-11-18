867208588951a52bb9bdf9312e354389ece5fd0b
chenzhao <chenzhao5001@163.com>
2022-11-18 19:56:53 +0800

    调整

--- studyproject/src/main/java/com/cz/studyproject/ui/IndexActivity.java
+++ studyproject/src/main/java/com/cz/studyproject/ui/IndexActivity.java
@@ -2,10 +2,13 @@ package com.cz.studyproject.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
+import com.cz.studyproject.MainActivity;
 import com.cz.studyproject.R;
 
 public class IndexActivity extends AppCompatActivity {
@@ -31,5 +34,19 @@ public class IndexActivity extends AppCompatActivity {
     }
 
     private void startMain() {
+        boolean isFirstApp = true;
+        Intent intent = new Intent();
+        if(isFirstApp) {
+            intent.setClass(this,GuideActivity.class);
+        } else {
+            String token = "";
+            if(token != "") {
+                intent.setClass(this, MainActivity.class);
+            } else {
+                intent.setClass(this,LoginActivity.class);
+            }
+        }
+        startActivity(intent);
+        finish();
     }
 }
